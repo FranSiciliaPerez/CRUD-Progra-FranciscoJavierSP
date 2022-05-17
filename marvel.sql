6e4001a29732694b6313b1e2fec6dba823eacfe1
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-05-17 14:21:47 +0200

    Add files via upload

--- marvel.sql
+++ marvel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2022 a las 14:18:42
+-- Tiempo de generación: 17-05-2022 a las 14:21:31
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.5
 
@@ -20,6 +20,54 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `marvel`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+
+CREATE TABLE `autores` (
+  `Codigo` int(11) NOT NULL,
+  `Nombre` varchar(30) COLLATE utf8_bin NOT NULL,
+  `Apellidos` varchar(30) COLLATE utf8_bin NOT NULL,
+  `Edad` int(11) NOT NULL,
+  `Superheroe` varchar(40) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `autores`
+--
+
+INSERT INTO `autores` (`Codigo`, `Nombre`, `Apellidos`, `Edad`, `Superheroe`) VALUES
+(7, 'fre', 'rfe', 432, 'fds');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `superheroes`
+--
+
+CREATE TABLE `superheroes` (
+  `Nombre` varchar(40) COLLATE utf8_bin NOT NULL,
+  `Apellidos` varchar(40) COLLATE utf8_bin NOT NULL,
+  `Edad` int(3) DEFAULT NULL,
+  `Alias` varchar(40) COLLATE utf8_bin NOT NULL,
+  `Superpoder` text COLLATE utf8_bin NOT NULL,
+  `Codigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `superheroes`
+--
+
+INSERT INTO `superheroes` (`Nombre`, `Apellidos`, `Edad`, `Alias`, `Superpoder`, `Codigo`) VALUES
+('Natasho', 'Romanoff', 87, 'Viuda negra', 'Suero super-soldado', 24),
+('Steve', 'Rogers', 87, 'Capitan America', 'Suero super-soldado', 25),
+('Bruce', 'Banner', 40, 'Hulk', 'Invulnersvilidad y regeneración celular', 26),
+('Peter', 'Parker', 25, 'Spiderman', 'sentido aracnido, super fuerza y adesividad', 27),
+('James ', 'Howlett', 100, 'Wolverine o Lobezno', 'Regeneración y huesos de adamantium', 28),
+('Luke', 'kage', 34, 'ff', 'cf', 29);
 
 -- --------------------------------------------------------
 
@@ -45,10 +93,38 @@ INSERT INTO `usuario` (`codUsuario`, `nomUsuario`, `contrasena`) VALUES
 --
 
 --
+-- Indices de la tabla `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`Codigo`);
+
+--
+-- Indices de la tabla `superheroes`
+--
+ALTER TABLE `superheroes`
+  ADD PRIMARY KEY (`Codigo`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`codUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `Codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `superheroes`
+--
+ALTER TABLE `superheroes`
+  MODIFY `Codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
